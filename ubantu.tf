586cf917d2b2a833df6b9df710b0052e39e549bb
shani <shanisingh098@gmail.com>
2021-04-24 12:32:26 +0530

    shayad ho jaye

--- ubantu.tf
+++ ubantu.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
     access_key = "AKIA2HGKR75RY2T7DDJD"
     secret_key = "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"
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-064446ad1d755489e"
+  ami           = "ami-08962a4068733a2b6"
   instance_type = "t2.micro"
-  key_name = var.ec2_keypair
+  key_name = "Vishnu1"
   count = var.ec2_count
   vpc_security_group_ids = ["${aws_security_group.ec2-sg.id}"]
-  subnet_id = element(var.subnets, count.index)
+
 
   tags = {
     Name = "${var.environment}-${count.index+1}"
@@ -20,14 +20,14 @@ resource "aws_instance" "ec2" {
 variable "ec2_count" {
   type = number
   default = "3"
-
+}
 variable "environment" {
   default = "Prod"
 }
 
 resource "aws_security_group" "ec2-sg" {
   name        = "${var.environment}-ec2-sg"
-  vpc_id      = aws_vpc.main.id
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -46,4 +46,3 @@ resource "aws_security_group" "ec2-sg" {
     Name = "ec2-sg"
   }
 }
-
