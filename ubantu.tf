cdf3ef4f25305555d7be769f5e424823071e1551
shani <shanisingh098@gmail.com>
2021-04-26 02:15:29 +0000

    shani

--- ubantu.tf
+++ ubantu.tf
@@ -6,9 +6,9 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "ami-08962a4068733a2b6"
   instance_type = "t2.micro"
-  key_name = "Vishnu1"
+  key_name = "dineshKeyFinal"
   count = var.ec2_count
-  vpc_security_group_ids = ["${aws_security_group.ec2-sg_10.id}"]
+  vpc_security_group_ids = ["${aws_security_group.ec2-sg_1.id}"]
 
 
   tags = {
@@ -18,14 +18,14 @@ resource "aws_instance" "ec2" {
 
 variable "ec2_count" {
   type = number
-  default = "4"
+  default = "3"
 }
 variable "environment" {
   default = "Prod"
 }
 
-resource "aws_security_group" "ec2-sg_10" {
-  name        = "${var.environment}-ec2-sg_10"
+resource "aws_security_group" "ec2-sg_1" {
+  name        = "${var.environment}-ec2-sg_1"
 
   ingress {
     from_port   = 22
@@ -42,6 +42,6 @@ resource "aws_security_group" "ec2-sg_10" {
   }
 
   tags = {
-    Name = "ec2-sg_10"
+    Name = "ec2-sg_1"
   }
 }
